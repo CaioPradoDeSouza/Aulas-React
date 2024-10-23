e35339c7c299a774881b5b4f7ccc5c215d38b84b
Caio Prado de Souza <cpssouza7@gmail.com>
2024-10-23 17:43:12 -0300

    update aulas de REACT JS

--- curso-react/src/componentes/Corpo.js
+++ curso-react/src/componentes/Corpo.js
@@ -1,9 +1,24 @@
 import React from "react"
+import Dados from "./Dados"
 
 export default function Corpo(){
+        const cnl=()=>{
+            return 'CFB Cursos'
+        }
+
+        const somar=(v1,v2)=>{
+            return v1+v2
+        }
+
     return (
         <div>
             <h2>Corpo</h2>
+            <Dados 
+                Nome={cnl} 
+                Idade={20} 
+                Sexo='masculino' 
+                somar={somar}
+            />
             <img src="./img/cachorro.webp"
             />
         </div>
--- curso-react/src/componentes/Dados.js
+++ curso-react/src/componentes/Dados.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 export default function Dados(props){
-
+    let n1 = 10
+    let n2 = 5
     return (
         <section>
             <h2>Dados</h2>
-            <p>Nome: {props.Nome}</p>
+            <p>Nome: {props.Nome()}</p>
             <p>Idade: {props.Idade}</p>
             <p>Sexo: {props.Sexo}</p>
+            <p>Somar: {props.somar(n1,n2)}</p>
         </section>
     )
 }
