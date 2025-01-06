c538fab9b6f7677b701d01328771b115db31d96c
Caio Prado de Souza <cpssouza7@gmail.com>
2025-01-06 17:54:58 -0300

    Feat: Update REACT class

--- curso-react/src/App.js
+++ curso-react/src/App.js
@@ -3,73 +3,30 @@ import React, { useState } from 'react';
 
 export default function App() {
 
-  // const msglogin=()=>{
-  //   return 'Usuário logado'
-  // }
+  const[form,setForm]=useState({"nome":"","curso":"","ano":""});
+  const handleFormChange = (e) =>{
+    if(e.target.getAttribute('name') == 'fnome'){
+      setForm({"nome":e.target.value,"curso":form.curso,"ano":form.ano});
+    }else if(e.target.getAttribute('name') == 'fcurso'){
+      setForm({"nome":form.nome,"curso":e.target.value,"ano":form.ano});
+    }else if(e.target.getAttribute('name') == 'fano'){
+      setForm({"nome":form.nome,"curso":form.curso,"ano":e.target.value});
+    }
+  }
 
-  // const msglogoff=()=>{
-  //   return 'favor logar'
-  // }
-
-  // const cumprimento=()=>{
-  //   const hora=new Date().getHours();
-  //   if(hora>=0 && hora<13){
-  //     return <p>Bom Dia!</p>
-  //   }else if(hora >= 13 && hora <18){
-  //     return <p>Boa Tarde!</p>
-  //   }else{
-  //     return <p>Boa Noite!</p>
-  //   }
-  // }
-
-  // const[cor,setCor]=useState(1);
-
-  // const vermelho={color:'#f00'}
-  // const verde={color:'#0f0'}
-  // const azul={color:'#00f'}
-
-  // const retCor=(c)=>{
-  //   if(c==1){
-  //     return vermelho
-  //   }else if(c==2){
-  //     return verde
-  //   }else{
-  //     return azul
-  //   }
-  // }
-
-  // const mudaCor=()=>{
-  //   setCor(cor+1)
-  //   if(cor > 2){
-  //     setCor(1)
-  //   }
-  // }
-
-  // setInterval(mudaCor,1000)
-
-  // const carros = ['HRV', 'GOLF', 'FOCUS', 'CRUZE', 'ARGO']
-
-  const carros = [
-    { categoria: "Esporte", preco: "11000.00", modelo: "golf gti" },
-    { categoria: "Esporte", preco: "11000.00", modelo: "i30" },
-    { categoria: "Esporte", preco: "11000.00", modelo: "civic" },
-    { categoria: "Esporte", preco: "11000.00", modelo: "ferrari" },
-  ];
-
-  const listaCarros = carros.map(
-    (c, i) =>
-      <li key={i}> {i} - {c.modelo} - {c.categoria} - R$ {c.preco}</li>
-
-  )
 
   return (
     <>
-      <h1>CFB CURSOS</h1>
-
-      <ul>{listaCarros}</ul>
-
-      {/* <h1 style={retCor(cor)}>CFB CURSOS</h1>
-      <button onClick={()=>{mudaCor()}}>Muda Cor</button> */}
+      <label>Nome</label>
+      <input type="text" name="fnome" value={form.nome}  onChange={(e)=>handleFormChange(e)}/><br/>
+      <label>Curso</label>
+      <input type="text" name="fcurso" value={form.curso}  onChange={(e)=>handleFormChange(e)}/><br/>
+      <label>Ano</label>
+      <input type="text" name="fano" value={form.ano}  onChange={(e)=>handleFormChange(e)}/><br/>
+      <p>Nome Digitado:{form.nome}</p>
+      <p>Curso Digitado:{form.curso}</p>
+      <p>Ano Digitado:{form.ano}</p>
+      
     </>
   );
 }
