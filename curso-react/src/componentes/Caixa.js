9585744151f871d38e2bf45d06a0dd3299a5b771
Caio Prado de Souza <cpssouza7@gmail.com>
2025-01-10 16:44:56 -0300

    Feat: update aulas JS

---
+++ curso-react/src/componentes/Caixa.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default function Caixa(props){
+    return(
+
+        <>
+            <p>{props.site}</p>
+            {props.children}
+
+        </>
+    )
+}
