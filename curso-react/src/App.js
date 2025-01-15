46a61a1656114fb40def916e85391d841c1db457
Caio Prado de Souza <cpssouza7@gmail.com>
2025-01-15 15:19:13 -0300

    Feat: update created class components

--- curso-react/src/App.js
+++ curso-react/src/App.js
@@ -1,30 +1,14 @@
-import React,{useState} from 'react';
-
+import React from 'react';
+import Classe from './componentes/Classe.js';
 
 
 export default function App() {
 
-  const [nome, setNome] = useState();
-
-  const armazenar=(chave,valor)=>{
-    localStorage.setItem(chave,valor);
-  }
-
-  const consultar=(chave)=>{
-    alert(localStorage.getItem(chave));
-  }
-
-  const apagar=(chave)=>{
-    localStorage.removeItem(chave);
-  }
 
   return (
     <>
-        <label>Digite um nome</label><br/>
-        <input type="text" value={nome} onChange={(e)=>setNome(e.target.value)}/><br/>
-        <button onClick={()=>armazenar('ls_nome',nome)} >Gravar Nome</button><br/>
-        <button onClick={()=>consultar('ls_nome')} >Ver Nome</button><br/>
-        <button onClick={()=>apagar('ls_nome')} >Remover Nome</button>
+        <h1>Componentes de Classe</h1>
+        <Classe canal="CFB cursos" curso="REACT"/>
     </>
   );
 }
--- curso-react/src/componentes/Canal.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-
-export default function Canal(){
-    return(
-
-        <h1>CFB cursos</h1>
-    )
-}
