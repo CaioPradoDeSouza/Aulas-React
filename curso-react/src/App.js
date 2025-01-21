8f8a336f2b5b3f9d41695bdd2bbe9c95079dd0ae
Caio Prado de Souza <cpssouza7@gmail.com>
2025-01-21 16:49:54 -0300

    Feat: Globals variables

--- curso-react/src/App.js
+++ curso-react/src/App.js
@@ -1,16 +1,39 @@
-import React,{useState} from 'react';
-import Carro from './componentes/Carro.js';
+import React, { useState } from 'react';
+import Globais from './componentes/Globais';
+import Info from './componentes/Info';
 
 
 
 export default function App() {
-  const[carro, setCarro]=useState(true);
-  const mostrarOcultarCarro=()=>{
-    setCarro(!carro)
+
+  const [resumo, setResumo]=useState(Globais.resumo)
+
+  const gravarResumo=()=>{
+    Globais.resumo=resumo
+  
   }
+
+  const verResumo=()=>{
+    alert(Globais.resumo)
+  }
+
   return (
     <>
-    </>    
+      <Info />
+
+
+      <p>{'Canal: ' + Globais.canal}</p>
+
+      <p>{'Curso: ' + Globais.curso}</p>
+
+      <p>{'Ano: ' + Globais.ano}</p>
+      <br/>
+      <input type='text' value={resumo} onChange={(e)=>setResumo(e.target.value)}/>
+      <br/>
+      <button onClick={()=>gravarResumo()}>Gravar Resumo</button>
+      <br/>
+      <button onClick={()=>verResumo()}>Ver Resumo</button>
+    </>
   );
 }
 
---
+++ curso-react/src/componentes/Globais.js
@@ -0,0 +1,8 @@
+
+
+export default class Globais {
+    static canal ="CFB cursos"
+    static curso ="React"
+    static ano="2025"
+    static resumo="Curso react cfb cursos"
+}
